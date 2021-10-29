b918a93fed2728ee33b21f22e20241502455ceb3
Areillee B <s11349941@students.duvalschools.org>
2021-10-29 14:37:48 -0400

    Update variables_practice.py

--- Variables/variables_practice.py
+++ Variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Areillee Butler, 10/29021 2:22pm, Version 0.0
+# Variables Practice, Areillee Butler, 10/29021 2:37pm, Version 0.1
+
+# Three Common Types of Data 
+
+lives_remaining = 13 # INTEGERS, any +/- Whole number and 0. 
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal. 
+death_cry = "Curse you, Rinoreus! I cannot stand you!"
+# Line 7 is a STRING  -- collection of letters/numbers/other chars.  
